a57de65aa967b721937c187355496c1412bc40f2
justin0kg <118658129+justin0kg@users.noreply.github.com>
2022-11-29 17:18:24 +0800

    support 32/64bit app for va

---
+++ app-plugin/src/main/java/io/busniess/va/addon/DebugActivity.java
@@ -0,0 +1,16 @@
+package io.busniess.va.addon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Lody
+ */
+public class DebugActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
